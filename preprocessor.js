c676d61174edacdbf246ada4420f548e2f9e036e
Dmitry Podlesny <dpodlesny@nwork.local>
2016-11-02 15:18:14 +0300

    preprocessor improve

--- preprocessor.js
+++ preprocessor.js
@@ -6,13 +6,15 @@ const config = require('./tsconfig.json');
 console.log(config);
 
 module.exports = {
-  process(src, path) {
-    if (path.endsWith('.ts') || path.endsWith('.tsx')) {
-      return tsc.transpile(src, {
-          module: tsc.ModuleKind.CommonJS,
-          jsx: tsc.JsxEmit.React,
-        }, path, []);
-    }
-    return src;
-  },
+	process(src, path) {
+		if (path.endsWith('.ts') || path.endsWith('.tsx')) {
+			return tsc.transpile(
+				src,
+				config.compilerOptions,
+				path,
+				[]
+			);
+		}
+		return src;
+	},
 };
