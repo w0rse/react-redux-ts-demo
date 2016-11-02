55fcd4ed42f9ae9140170d72c0715e94d2eaffdd
Dmitry Podlesny <dpodlesny@nwork.local>
2016-11-02 17:16:57 +0300

    Active Tool Reducer Test

--- client/reducers/activeTool.test.ts
+++ client/reducers/activeTool.test.ts
@@ -1,3 +1,33 @@
-it('adds 1 + 2 to equal 3 in ts', () => {
-  expect(1 + 2).toBe(3);
+import { activeTool as reducer } from './activeTool';
+import { SET_ACTIVE_TOOL } from '../constants/actions';
+import { TOOLS } from '../constants/tools';
+
+describe('Active Tool Reducer', () => {
+
+	it('Should handle SET_ACTIVE_TOOL', () => {
+
+		// Set ARROW
+		expect(
+			reducer(
+				'',
+				{
+					type: SET_ACTIVE_TOOL,
+					payload: TOOLS.ARROW
+				}
+			)
+		).toEqual(TOOLS.ARROW);
+
+		// Set BRUSH
+		expect(
+			reducer(
+				TOOLS.ARROW,
+				{
+					type: SET_ACTIVE_TOOL,
+					payload: TOOLS.BRUSH
+				}
+			)
+		).toEqual(TOOLS.BRUSH);
+
+	});
+
 });
